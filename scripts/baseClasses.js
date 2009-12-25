41c7003004498885deb501fe8ced038e861c5a60
dotnetcarpenter <jon.ronnenberg@gmail.com>
2009-12-25 03:38:51 +0100

    renamed classes.js to baseClasses.js

---
+++ scripts/baseClasses.js
@@ -0,0 +1,50 @@
+/**
+ * @author Jon Ege Ronnenberg
+ * @version 0.1
+ * Property names is to the fullest extent taken from the blog post 'NEWSPAPER JARGON'
+ * http://vasukibelavadi.wordpress.com/2006/08/06/newspaper-jargon/
+ */
+var Teeboa = {};
+/**
+ * An article
+ * @classDescription A Teeboa article.
+ * @param {Object} config
+ */
+Teeboa.Article = function(config){
+	this.id = config.id;
+	this.headline = config.article_headline;
+	this.byLine = config.journalist_name;
+	this.deck = config.article_summary;
+	this.content = config.article_body_text;
+	this.dateline = config.article_timestamp;
+	this.providerName = config.supplier_name;
+	this.providerLogo = config.supplier_logo;
+	this.topicName = config.topic_name;
+	this.purchaseTimestamp = config.purchase_timestamp;
+	this.purchasePrice = config.purchase_timestamp;
+};
+/**
+ * A user. Singleton
+ * @param {Object} config
+ */
+Teeboa.Customer = function(config){
+	this.id = config.id;
+}();
+/**
+ * A provider.
+ * @param {Object} config
+ */
+Teeboa.Provider = function(config){
+	this.id = config.supplier_id;
+	this.name = config.supplier_name;
+	this.logo = config.supplier_logo;
+};
+/**
+ * A transaction.
+ * @param {Object} config
+ */
+Teeboa.Transaction = function(config){
+	this.date = config.transaction_date;
+	this.text = config.transaction_text;
+	this.amount = config.transaction_amount;
+};
