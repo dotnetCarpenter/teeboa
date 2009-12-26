d02080c284647666b7bcf2931ba0b68525c21802
dotnetcarpenter <jon.ronnenberg@gmail.com>
2009-12-26 03:50:35 +0100

    change char case for hdStore ids

--- scripts/classes.js
+++ scripts/classes.js
@@ -7,17 +7,17 @@
  * Singleton
  */
 Teeboa.Providers = function(){
-	return new hdStore('Providers');
+	return new hdStore('providers');
 }();
 /**
  * Articles collection.
  * Singleton
  */
 Teeboa.Articles = function(){
-	return new hdStore('Articles');
+	return new hdStore('articles');
 }();
 /**
  * Transaction collection.
  * Singleton
  */
-Teeboa.Transactions = function(){ return new hdStore('Transactions'); }();
+Teeboa.Transactions = function(){ return new hdStore('transactions'); }();
