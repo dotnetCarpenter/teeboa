a4782a4ac334f9b463475594f5a9df99f9c91e2e
Marc Falk <marc.falk@gmail.com>
2010-01-19 22:19:19 +0100

    added jquery 1.4

--- html/include/header.php
+++ html/include/header.php
@@ -1,35 +1,69 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta name="author" content="teeboa" />
-	<title>Teeboa</title>
-	<link type="text/css" rel="stylesheet" href="style/stylesheet.css" />
-	<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
-	<script type="text/javascript" src="js/jquery.masonry.js"></script>
-</head>
-
-<body>
-
-<div>
-
-	<!-- HEADER -->
-
-	<div id="header">
-		<div class="headerContainer"><h1></h1></div>
-		<div class="headerLayer"></div>
-		<div class="headerMenu widthWrap">
-			<a href="index.php">min avis</a>
-			<a href="criteria.php">kriterier</a>
-			<a href="settings.php">indstillinger</a>
-			<a href="account.php">konto</a>
-			<a href="#" class="filterButtonAdd"></a>
-		</div>
-	</div>
-	<div class="headerUnderline">&nbsp;</div>
-	
-	<!-- CONTENT -->
-	
-	<div id="content">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+
+
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+	<meta name="author" content="teeboa" />
+
+	<title>Teeboa</title>
+
+	<link type="text/css" rel="stylesheet" href="style/stylesheet.css" />
+
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+	<script type="text/javascript" src="js/jquery.masonry.js"></script>
+
+</head>
+
+
+
+<body>
+
+
+
+<div>
+
+
+
+	<!-- HEADER -->
+
+
+
+	<div id="header">
+
+		<div class="headerContainer"><h1></h1></div>
+
+		<div class="headerLayer"></div>
+
+		<div class="headerMenu widthWrap">
+
+			<a href="index.php">min avis</a>
+
+			<a href="criteria.php">kriterier</a>
+
+			<a href="settings.php">indstillinger</a>
+
+			<a href="account.php">konto</a>
+
+			<a href="#" class="filterButtonAdd"></a>
+
+		</div>
+
+	</div>
+
+	<div class="headerUnderline">&nbsp;</div>
+
+	
+
+	<!-- CONTENT -->
+
+	
+
+	<div id="content">
+
 		<div class="widthWrap">
