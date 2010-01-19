b4e21cd803501481c4f28ebd2e821e3d240c340b
Marc Falk <marc.falk@gmail.com>
2010-01-19 21:25:18 +0100

    html for prototype

---
+++ html/article.php
@@ -0,0 +1,15 @@
+<?php include('include/header.php'); ?>
+
+			<div class="article">
+				<h2>DR-journalist vinder Cavling-prisen</h2>
+				<p class="summary">DR's reporter Jesper Tynell vinder Cavling-prisen 2009. Han får den for sin dækning af lovbrud i Beskæftigelsesministeriet.</p>
+				<p class="full">For anden gang på under to måneder kunne Jesper Tynell fra Orientering på P1 onsdag sikre sig en fornem pris for sit journalistiske gravearbejde i den såkaldte "kontanthjælpssag".<br /><br />I november blev han hædret af Foreningen for Undersøgende Journalistik for at være årets bedste "graver" på radio. Og onsdag blev det endnu finere, da Dansk Journalistforbund besluttede sig, at Jesper Tynell skal have journalisternes mest fornemme pris, Cavling-prisen 2009.<br /><br /><b>Holder lytterne fast</b><br />"Han har i en række indslag i Orientering på P1 dokumenteret, hvordan daværende beskæftigelsesminister Claus Hjort Frederiksen og hans ministerium brød loven, bestilte misvisende tal, slettede belastende dokumenter og vildledte Folketinget," lyder det i begrundelsen fra Cavling-komitéen.<br /><br />Tynell har ifølge Cavling-komitéen blandt andet påvist, at Claus Hjort Frederiksen uden om Folketinget afskaffede kravene til udenlandske arbejdstageres sikkerhed i farlige job. Ministeren pålagde ulovligt kommunerne at standse udbetalingen af kontanthjælp til mennesker, der ikke var omfattet af den såkaldte 300-timers-regel. Ministeren bestilte statistikker hos sine embedsmænd, der skulle vise, at kommunerne var bedre end arbejdsformidlingerne til at få ledige i arbejde. Og han iværksatte forsøg med tvangsaktivering af sygemeldte uden først at spørge Folketinget.<br /><br /><b>Aldrig tæt på Claus Hjort</b><br />I de 15 indslag, det er blevet til i Orientering på P1 om sagen, har Jesper Tynell dokumenteret, hvordan Beskæftigelsesministeriet har brugt den såkaldte 300-timers regel til at standse udbetalingen af kontanthjælp til familier, der ifølge loven slet ikke er omfattet af reglen. Ministeriet har da også efterfølgende ændret praksis, mens det aldrig lykkedes for Jesper Tynell at få Claus Hjort Frederiksen i tale i sagen.<br /><br />Der var i alt 68 forslag til Cavling-prisen i år, hvoraf de 36 var forskellige.</p>
+				<div class="divider"></div>
+				<p class="author">
+					<span class="icon berlingske"></span>
+					<span class="text">Af <span>Søren Påske</span>, Onsdag d. 25. November 2009, 20:49</span>
+					<a class="back" href="javascript: history.go(-1)"></a>
+				</p>
+			</div>
+			
+<?php include('include/footer.php'); ?>
---
+++ html/include/footer.php
@@ -0,0 +1,18 @@
+		</div>
+	</div>
+	
+	<!-- FOOTER -->
+	
+	<div id="footer">
+		<div class="widthWrap">
+			
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+$('#grid').masonry({ columnWidth: 330 , singleMode: true });
+</script>
+
+</body>
+</html>
