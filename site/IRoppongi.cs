cb5bb538bc39cc80ec2f11e50a925b110f11e985
dotnetcarpenter <jon.ronnenberg@gmail.com>
2010-02-11 18:11:17 +0100

    file on the Amager server

---
+++ site/IRoppongi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+using System.IO;
+using System.ServiceModel.Web;
+
+namespace RoppongiAPI
+{
+    [ServiceContract]
+    interface IRoppongi
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "{CustomerId}/Articles")]
+        Stream GetArticles(string CustomerId);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "{CustomerId}/article{ArticleId}/Buy")]
+        Stream BuyArticle(string CustomerId, string ArticleId);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "{CustomerId}/article{ArticleId}")]
+        Stream GetArticle(string CustomerId, string ArticleId);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "{CustomerId}/Providers")]
+        Stream GetProviders(string CustomerId);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "{CustomerId}/Filters")]
+        Stream GetFilters(string CustomerId);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "{CustomerId}/Filter/Update?filterid={FilterId}&providerid={ProviderId}&topicid={TopicId}", Method = "POST", BodyStyle = WebMessageBodyStyle.Bare)]
+        void AddFilter(string CustomerId, string FilterId, string ProviderId, string TopicId);
+        
+        [OperationContract]
+        [WebInvoke(UriTemplate = "{CustomerId}/Filter/Update?filterid={FilterId}&providerid={ProviderId}&topicid={TopicId}", Method = "DELETE")]
+        void RemoveFilter(string CustomerId, string FilterId, string ProviderId, string TopicId);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "{CustomerId}/Update?name={name}&email={email}", Method = "PUT")]
+        Stream UpdateCustomer(string CustomerId, string name, string email);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "{CustomerId}/Account")]
+        Stream GetTransactions(string CustomerId);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "{CustomerId}/Account/Balance")]
+        Stream GetBalance(string CustomerId);
+    }
+}
