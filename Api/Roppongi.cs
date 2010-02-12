caef887b8e8e7b2895e7f8d1d5e8f73064407cf5
dotnetcarpenter <jon.ronnenberg@gmail.com>
2010-02-12 03:47:47 +0100

    committed API

---
+++ Api/Roppongi.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using FirebirdSql.Data.FirebirdClient;
+using System.IO;
+using System.ServiceModel.Web;
+
+namespace Roppongi.Api
+{
+    // NOTE: If you change the class name "Roppongi" here, you must also update the reference to "Roppongi" in Web.config and in the associated .svc file.
+    public partial class Roppongi : IRoppongi
+    {
+        #region IRoppongi Members
+
+        public Stream GetArticles(string CustomerId)
+        {
+            FbParameter cust = new FbParameter("customer_id", CustomerId);
+            FbParameter filter = new FbParameter("filter", null);
+
+            WebOperationContext.Current.OutgoingResponse.ContentType = "text/html";
+            return new MemoryStream(Encoding.UTF8.GetBytes(GetJson("article_list", "articles", cust, filter)));
+        }
+
+        public Stream BuyArticle(string CustomerId, string ArticleId)
+        {
+            FbParameter cust = new FbParameter("customer_id", CustomerId);
+            FbParameter article = new FbParameter("article_id", int.Parse(ArticleId));
+
+            WebOperationContext.Current.OutgoingResponse.ContentType = "text/html";
+            return new MemoryStream(Encoding.UTF8.GetBytes(GetJson("article_purchase", cust, article)));
+        }
+
+        public Stream GetArticle(string CustomerId, string ArticleId)
+        {
+            FbParameter cust = new FbParameter("customer_id", int.Parse(CustomerId));
+            FbParameter article = new FbParameter("article_id", int.Parse(ArticleId));
+
+            WebOperationContext.Current.OutgoingResponse.ContentType = "text/html";
+            return new MemoryStream(Encoding.UTF8.GetBytes(GetJson("article_detail", cust, article)));
+        }
+
+        public Stream GetProviders(string CustomerId)
+        {
+            WebOperationContext.Current.OutgoingResponse.ContentType = "text/html";
+            return new MemoryStream(Encoding.UTF8.GetBytes(GetJson("supplier_list", "providers")));
+        }
+
+        public Stream GetFilters(string CustomerId)
+        {
+            FbParameter cust = new FbParameter("customer_id", CustomerId);
+
+            WebOperationContext.Current.OutgoingResponse.ContentType = "text/html";
+            return new MemoryStream(Encoding.UTF8.GetBytes(GetJson("customer_criteria_list", cust)));
+        }
+
+        public void AddFilter(string CustomerId, string FilterId, string ProviderId, string TopicId)
+        {
+            throw new NotImplementedException();
+            //TODO: throw HTTP error codes if failure
+        }
+
+        public void RemoveFilter(string CustomerId, string FilterId, string ProviderId, string TopicId)
+        {
+            throw new NotImplementedException();
+            //TODO: throw HTTP error codes if failure
+        }
+
+        public Stream UpdateCustomer(string CustomerId, string name, string email)
+        {
+            throw new NotImplementedException();
+            //TODO: throw HTTP error codes if failure
+        }
+
+        public Stream GetTransactions(string CustomerId)
+        {
+            FbParameter cust = new FbParameter("customer_id", CustomerId);
+
+            WebOperationContext.Current.OutgoingResponse.ContentType = "text/html";
+            return new MemoryStream(Encoding.UTF8.GetBytes(GetJson("customer_account_statement", cust)));
+        }
+
+        public Stream GetBalance(string CustomerId)
+        {
+            FbParameter cust = new FbParameter("customer_id", CustomerId);
+
+            WebOperationContext.Current.OutgoingResponse.ContentType = "text/html";
+            return new MemoryStream(Encoding.UTF8.GetBytes(GetJson("customer_account_balance", cust)));
+        }
+
+        #endregion
+    }
+}
---
+++ Library/TeeboaConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FirebirdSql.Data.FirebirdClient;
+
+namespace Roppongi.Library
+{
+    public static class TeeboaConnection
+    {
+        // Properties
+        public static FbConnectionStringBuilder ConnectionString
+        {
+            get
+            {
+                FbConnectionStringBuilder conn = new FbConnectionStringBuilder("User=TEEBOA;Password=#teeboa!;Database=Teeboa;DataSource=melabos.dk;");
+                conn.Port = 0x32fc;
+                return conn;
+            }
+        }
+    }
+}
