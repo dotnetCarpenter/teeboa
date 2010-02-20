10c4f43939a262afac4055969dd1d6201b86038f
dotnetcarpenter <jon.ronnenberg@gmail.com>
2010-02-20 21:30:42 +0100

    removed jquery project

--- 3rd parties libs/jquery/src/sizzle-jquery.js
+++
@@ -1,9 +0,0 @@
-jQuery.find = Sizzle;
-jQuery.expr = Sizzle.selectors;
-jQuery.expr[":"] = jQuery.expr.filters;
-jQuery.unique = Sizzle.uniqueSort;
-jQuery.getText = getText;
-jQuery.isXMLDoc = isXML;
-jQuery.contains = contains;
-
-return;
